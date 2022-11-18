49822ac1125dc57c8c9a59c06839b0c704bb6d9f
B3ns2005 <60721397+bkrstrawson@users.noreply.github.com>
2022-11-18 11:55:55 -0600

    started figureing out what i need to do

--- proposal.md
+++ proposal.md
@@ -1,9 +1,18 @@
 # Proposal
 
-adventure game
+tower defense 
 
 ## Needs to hvae list
-
+5 levels
+10 different enemies (knights)
+10 towers with dif abilities(dragons)
+all towers target first enemy they can reach
+shop that upgrades damage and fire speed
 
 ## Nice to have list 
+shop that adds abilities 
+enemis with abilies
+more complex targetting 
+
+
 
--- sketch.js
+++ sketch.js
@@ -5,6 +5,47 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+class Tower{
+  constructor(x,y,damage,ability,bulletSpeed,firespeed,range){
+    this.x = x
+    this.y = y
+    this.bulletSpeed= bulletSpeed
+    this. damage = damage
+    this.firespeed = firespeed
+    this.ability= ability
+  }
+  target(){//decides what enemy to shoot at and prompts the shoot
+
+  }
+  shoot(){//shoots bullet(creates a new bullet object)
+
+  }
+
+  }
+
+  class Bullet{
+    constructor(x,y,speed,damage,range){
+      this.x = x
+      this.y = y
+      this.spped = speed
+      this.damage = damage
+
+    }
+  }
+
+  class Enemy{
+    constructor(x,y,movementSpeed, health,damge){
+      this.x = x
+      this.y = y
+      this.movementSpeed = movementSpeed
+      this.health = health
+      this.damage
+    }
+
+    move(){//moves and turns each enemy
+
+    }
+  }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +53,6 @@ function setup() {
 
 function draw() {
   background(220);
+  fill("black")
+  circle(10,10,10)
 }
