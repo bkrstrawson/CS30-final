776fda798b94b596e84aa47c4c5290a0251528e2
B3ns2005 <60721397+bkrstrawson@users.noreply.github.com>
2022-11-28 12:17:19 -0600

    added shoot functiion

--- sketch.js
+++ sketch.js
@@ -9,6 +9,7 @@ let towerAR = [];
 let bulletAR = [];
 let enemyAR= [];
 let bullet;
+let circles;
 
 class Tower{
   constructor(x,y,damage,ability,bulletSpeed,firespeed,range,color){
@@ -17,7 +18,7 @@ class Tower{
     this.bulletSpeed= bulletSpeed;
     this. damage = damage;
     this.firespeed = firespeed;
-    this.coolDown = 0
+    this.coolDown = 0;
     this.ability= ability;
     this.range = range;
     //this.imagefile = imagefile
@@ -29,17 +30,31 @@ class Tower{
   target(){//decides what enemy to shoot at and prompts the shoot
     let targetprogress;
     let target;
+    this.targetX=0;
+    this.targetY=0;
     for(let i = enemyAR.length; i >0; i--){
-      
-      if (targetprogress < enemyAR[i].Progress){
-        targetprogress = enemyAR[i].Progress;
-        target = i;
+      if(dist(enemyAR[i].X(),enemyAR[i].Y(),this.x,this.y) <= this.range){
+        if (targetprogress < enemyAR[i].Progress){
+          targetprogress = enemyAR[i].Progress;
+          target = i;
+        }
       }
     }
     this.targetX = enemyAR[target].X();
     this.targetY = enemyAR[target].Y();
   }
   shoot(){//shoots bullet(creates a new bullet sprite)
+    if(this.targetX !== 0 || this.targetY !== 0){
+      if (this.coolDown !==0){
+        this.coolDown--;
+      }
+      else{
+        this.coolDown = this.firespeed;
+        bullet =new Sprite(this.x,this.y,10,"static");
+        bullet.moveTO(this.targetX,this.targetY,this.bulletSpeed);
+        bulletAR.push(bullet);
+      }
+    }
     
   } 
   display(){
@@ -101,12 +116,18 @@ class Enemy{
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  bullet = new Sprite(); 
-  bullet.height = 50;
-  bullet.width = 50;
+
+  bullet = new Sprite(400,400,10);
+
+  bullet.color = "red";
+  
+ 
   
 }
+
+  
 function mousePressed(){
+  bullet.moveTO(0,0,10);
   bullet.moveTo(mouseX,mouseY,3);
 }
 
@@ -130,7 +151,7 @@ function update(){
 }
 
 function trig(x1,y1,x2,y2){
-  let x = abs(x2-x1)
-  let y = abs(y2-y1)
-  let distance = x*x + y*y 
+  let x = abs(x2-x1);
+  let y = abs(y2-y1);
+  let distance = x*x + y*y; 
 }
