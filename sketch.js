397886154597915c54233966353bb30196cc8864
B3ns2005 <60721397+bkrstrawson@users.noreply.github.com>
2023-01-16 17:45:33 -0600

    added levels and towers

--- sketch.js
+++ sketch.js
@@ -15,28 +15,33 @@ let button;
 let gamestate = "title";
 let buttonAR= [];
 let target = 0;
-let movePoints = [{x:-50, y:75},{x:1475,y:75},{x:1475,y:705},{x:75,y:705}, {x:75, y:400}, {x:1600, y:400}];
+let movePoints = [{x:-50, y:700},{x:400,y:700},{x:400, y:75},{x:1000,y:75},{x:1000,y:600},{x:1475, y:600},{x:1475,y:-50}]
+let level1 = [{x:-50, y:75},{x:1475,y:75},{x:1475,y:705},{x:75,y:705}, {x:75, y:400}, {x:1600, y:400}];
+let level2 = [{x:-50, y:700},{x:400,y:700},{x:400, y:75},{x:1000,y:75},{x:1000,y:600},{x:1475, y:600},{x:1475,y:-50}]
+let level3 = [{x:200,y:820}, {x:200,y:75},{x:1475, y:75}, {x:1475,y:820}]
+let level4 = [{x:175, y:-20}, {x:175, y:390}, {x:1575, y:390}]
+let level5 = [{x:-20, y:390}, {x:1575, y:390}]
 let index,levels,levelname,level;
 let wallHealth = 2000;
 let paths = [];
 let previousState = "none";
 let temptower;
 let sometime = 0;
-let money = 0;
+let money = 500;
 
 function proload(){
   levels = loadStrings("levels/levels");
 }
 
 class Tower{
-  constructor(x,y,damage,ability,bulletSpeed,firespeed,range,color){
+  constructor(x,y,damage,price,bulletSpeed,firespeed,range,color){
     this.x = x;
     this.y = y;
     this.bulletSpeed= bulletSpeed;
     this. damage = damage;
     this.firespeed = firespeed;
     this.coolDown = 0;
-    this.ability= ability;
+    this.price = price
     this.range = range;
     //this.imagefile = imagefile
     this.color = color;
@@ -69,8 +74,8 @@ class Tower{
         }
       }
     }
-    console.log(this.targetX + "x");
-    console.log(this.targetY + "y");
+    //console.log(this.targetX + "x");
+    //console.log(this.targetY + "y");
   }
 
   shoot(){//shoots bullet(creates a new bullet sprite)
@@ -84,7 +89,7 @@ class Tower{
         
       }
       else{
-        this.coolDown--;
+        this.coolDown--
       }
       
     } 
@@ -114,7 +119,7 @@ class Bullet{
 }
 
 class Enemy{
-  constructor(x,y,movementSpeed,health,damage){
+  constructor(x,y,movementSpeed,health,damage,money){
     this.sprite = new Sprite();
     this.sprite.x = x;
     this.sprite.y = y;
@@ -126,6 +131,7 @@ class Enemy{
     this.sprite.collider = "k";
     this.n = 0;
     this.sprite.moveTo(movePoints[0],this.movementSpeed/5);
+    this.money = money
   }
 
   switchDirection(){//switches direction when hit a wall
@@ -140,8 +146,8 @@ class Enemy{
       }
 
     }
-    console.log(this.sprite.x + "ex")
-    console.log(this.sprite.y + "ey")
+    //console.log(this.sprite.x + "ex")
+    //console.log(this.sprite.y + "ey")
     this.progress+= this.movementSpeed;
   }
   takesDamage(damageTaken){
@@ -156,7 +162,7 @@ function setup() {
   // let enemy1 = new Enemy(50,50,1,100,3,"right");
   // enemyAR.push(enemy1);
   drawPaths();
-  let enemy1 = new Enemy(movePoints[0].x,movePoints[0].y,10,1000,3);
+  let enemy1 = new Enemy(movePoints[0].x,movePoints[0].y,10,1000,3,1000);
   enemyAR.push(enemy1);
 
   //perm
@@ -166,7 +172,17 @@ function setup() {
   buttonAR.push(button);
   button = new Button(width-100,height-50,200,100,"green","orange","game","shop","back");//back
   buttonAR.push(button);
-  button = new Button(width/6*2,height/6*2,200,100,"blue","red","tower","shop","tower 1");//buy tower 1
+  button = new Button(387,100,200,100,"blue","red","tower1","shop","tower 1");//buy tower 1
+  buttonAR.push(button);
+  button = new Button(775,100,200,100,"blue","red","tower2","shop","tower 2");//buy tower 2
+  buttonAR.push(button);
+  button = new Button(387,250,200,100,"blue","red","tower3","shop","tower 3");//buy tower 3
+  buttonAR.push(button);
+  button = new Button(775,250,200,100,"blue","red","tower4","shop","towe r4");//buy tower 4
+  buttonAR.push(button);
+  button = new Button(387,400,200,100,"blue","red","tower5","shop","tower 5");//buy tower 5
+  buttonAR.push(button);
+  button = new Button(775, 400,200,100,"blue","red","tower6","shop","tower6");//buy tower 6
   buttonAR.push(button);
 }
 
@@ -176,19 +192,24 @@ function draw() {
   buttonsupdate();
   if (gamestate === "game"){
     update();
-    perSecond();
     showpaths();
   }
-  
   else if(gamestate === "tower"){
     update();
     displayTower();
-    perSecond();
     showpaths();
   }
+  else if( gamestate[2] ==="w"){
+    checkTower()
+  }
+
   else{
     turnOffGame();
     hidepaths();
+  }
+  if (gamestate !== "title"){
+    displayMoney()
+    perSecond();
   }
   previousState = gamestate;
 }
@@ -214,6 +235,7 @@ function update(){
     enemyAR[i].sprite.visible = true;
     enemyAR[i].switchDirection();
     if(enemyAR[i].health <= 0){
+      money += enemyAR[i].money
       enemyAR[i].sprite.remove();
       enemyAR.splice(i,1);
       
@@ -243,13 +265,10 @@ function turnOffGame(){
 
 let pathCollide = false;
 function displayTower(){// potentailly monkey code
-  if (previousState === "shop"){
-    temptower = new Tower(mouseX,mouseY,50,"none",9,25,500,"red");
-  }
   temptower.x = mouseX;
   temptower.y = mouseY;
   temptower.display();
-  pathCollide = false;
+  //pathCollide = false;
   // for (let i = 0; i < paths.length; i ++){
   //   console.log(temptower.sprite.overlapping(paths[3]) > 0 && !pathCollide);
   //   if (temptower.sprite.overlapping(paths[i]) > 0 && !pathCollide){
@@ -268,16 +287,20 @@ function displayTower(){// potentailly monkey code
 
 function mousePressed(){
   if (gamestate === "tower"){
+    if(money >= temptower.price){
     // for (let i = 0; i < paths.length; i ++){
     // if (temptower.sprite.overlapping(paths[i])){
-    // towerAR.push(temptower);
-    // temptower = "none";
+     towerAR.push(temptower);
+     money -= temptower.price
+     temptower = "none";
+     gamestate = "game";
+     
+    }
+    //}
+    //}
+    // let tower = new Tower(mouseX,mouseY,50,"none",9,25,500,"red");
+    // towerAR.push(tower);
     // gamestate = "game";
-    //}
-    //}
-    let tower = new Tower(mouseX,mouseY,50,"none",9,25,500,"red");
-    towerAR.push(tower);
-    gamestate = "game";
   }
 
   for(let i = buttonAR.length-1; i >= 0; i --){
@@ -396,6 +419,39 @@ function perSecond(){//happens 1 time per second
   if (millis() >=sometime){
     money += 5;
     sometime = millis() +1000;
-    
   }
 }
+function displayMoney(){
+  fill(255)
+    textSize(32);
+    rect(0,0,textWidth(money)+75,65);
+    stroke(0);
+    fill(0);
+    textSize(8);
+    textSize(32);
+    text(money, 10, 35);
+    fill("white"); 
+    //image(imgBanana,textWidth(banana1)+20,0,50,50);
+  }
+
+  function checkTower(){
+    if(gamestate === "tower1"){// cheap tower, slow firing, mid damage, kid range,
+      temptower = new Tower(mouseX,mouseY, 15, 50, 8,20,400,"blue")
+    }
+    else if (gamestate === "tower2"){//more expensive, faster firing, mid damge, higher range
+      temptower = new Tower(mouseX,mouseY,20, 100, 10, 25,500,"green")
+    }
+    else if (gamestate === "tower3"){// high damge slow firing 
+      temptower = new Tower(mouseX,mouseY,50,250,8,10,500,"red")
+    }
+    else if(gamestate === "tower4"){// low damge high fire speed
+      temptower = new Tower(mouseX,mouseY, 5,250,12,5,500,"orange")
+    }
+    else if (gamestate === "tower5"){//super tower
+      temptower = new Tower(mouseX,mouseY,40,500,12,12,600,"purple")
+    }
+    else if(gamestate === "tower6"){// one shot man
+      temptower = new Tower(mouseX,mouseY, 10000,500, 12,1000,400,"black")
+    }
+    gamestate = "tower"
+  }
